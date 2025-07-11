94fd8f615b616838a4a67fddc6960ced5504efef
iurii <immirg@gmail.com>
2025-07-11 18:23:12 +0300

    Interfaces task 2

---
+++ src/main/java/org/example/CSVConvert.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface CSVConvert {
+    public String toCSVString(Student student);
+    public Student fromCSVString (String str);
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student(11, "Group1", "Daisy", "Dove", Gender.female);
-        Student student2 = new Student(12, "Group1", "Alice", "Amelia", Gender.female);
-        Student student3 = new Student(13, "Group1", "Mark", "Matthew", Gender.male);
-        Student student4 = new Student(14, "Group1", "James", "Joseph", Gender.male);
+        String groupName = "Group_1";
+        Student student1 = new Student(11, groupName, "Daisy", "Dove", Gender.female);
+        Student student2 = new Student(12, groupName, "Alice", "Amelia", Gender.female);
+        Student student3 = new Student(13, groupName, "Mark", "Matthew", Gender.male);
+        Student student4 = new Student(14, groupName, "James", "Joseph", Gender.male);
 
         Group group = new Group();
-        String groupName = "New group";
         addNewStudentToGroup(group, student1, groupName);
         addNewStudentToGroup(group, student2, groupName);
         addNewStudentToGroup(group, student3, groupName);
@@ -24,6 +24,12 @@ public class Main {
 
         addNewStudentToGroup(group, student5, groupName);
         System.out.println(student5);
+
+        Student student = new Student();
+        System.out.println(student5.toCSVString(student5));
+
+        Student studentFromFile = student.fromCSVString("student.csv");
+        System.out.println(studentFromFile);
     }
     public static void addNewStudentToGroup(Group group, Student student, String groupName) {
         try {
